3a23c14999786d9e074e46e6ade94c2a8b7f02ed
kavindu Dhananjaya <dhananjayaa1998@gmail.com>
2020-10-31 15:32:34 +0530

    add dependency

---
+++ src/main/java/com/ijse/spring_boot_02/MyRestController.java
@@ -0,0 +1,21 @@
+package com.ijse.spring_boot_02;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class MyRestController {
+
+    @GetMapping(path = "/hello", produces = "text/plain")
+    public ResponseEntity<Object> sayHello() {
+
+        return new ResponseEntity<Object>("Hello Spring Boot ", HttpStatus.OK);
+    }
+}
+
