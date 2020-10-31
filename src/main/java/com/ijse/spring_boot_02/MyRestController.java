665bde2cd71a28bf5ef43c2b23f1b4fdcbe5c963
kavindu Dhananjaya <dhananjayaa1998@gmail.com>
2020-10-31 15:35:07 +0530

    create rest controller

--- src/main/java/com/ijse/spring_boot_02/MyRestController.java
+++ src/main/java/com/ijse/spring_boot_02/MyRestController.java
@@ -1,11 +1,10 @@
 package com.ijse.spring_boot_02;
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api")
@@ -18,4 +17,5 @@ public class MyRestController {
         return new ResponseEntity<Object>("Hello Spring Boot ", HttpStatus.OK);
     }
 }
+
 
